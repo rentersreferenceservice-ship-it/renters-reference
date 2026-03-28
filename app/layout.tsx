43b2e49cc0624f0b1069e86ad530273c765f81fe
RentersReference <rentersreferenceservice@gmail.com>
2026-03-28 17:00:43 -0400

    force analytics redeploy test

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
-
+import { Analytics } from "@vercel/analytics/next";
+{/* analytics test */}
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
